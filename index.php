c6a7322f4d8838decd9c89e1e0b0947031802301
yh72167 <yh.19972372@gmail.com>
2020-08-22 16:44:17 +0900

    [add]最初のコミット

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>刃牙vs最凶死刑囚</title>
+    <link type="text/css" rel="stylesheet" href="css/reset.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="content-wrap">
+        <header class="index-header">
+            <img class="header-logo-img" src="img/logo.png">
+            <h1 class="header-logo-title"">地下格闘技戦士VS最強死刑囚</h1>
+        </header>
+        <main class="index-main">
+            <h1 class="index-message">「敗北を知りたい」</h1>
+            <img class="main-logo-img" src="img/shikeishu.jpg">
+        </main>
+            <section class="selection">
+                <h1 class="panel-title">東京にどえらい連中が上陸するッ!!</h1>
+                <p class="panel-text">グラップラーを選択するのじゃッ!!</p>
+                <form class="character-select" method="post" action="">
+                    <div class="character-wrap">
+                        <img class="character-panel" src="img/fighter01.png">
+                        <img class="character-panel" src="img/fighter02.png">
+                        <img class="character-panel" src="img/fighter03.png">
+                        <img class="character-panel" src="img/fighter04.png">
+                        <img class="character-panel" src="img/fighter05.png">
+                    </div>
+                    <input class="btn btn-start btn-inactive" type="submit" name="start" value="選べッ!!">
+                </form>
+            </section>
+    </div>
+    <footer class="footer-link">
+        <p class="tv-link">TVアニメ「バキ」公式サイトへッ!!</p>
+        <p>このゲームは「バキ」のファンサイトです。</p>
+        公式とは一切関係ありません。
+    </footer>
+    <!-- jqueryの読み込み(scr以降の物はおまじないと思っておいて良いかも) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
